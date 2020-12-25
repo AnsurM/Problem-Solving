fbd88efdc0c00fb882e581aed43ea66e52db9322
AnsurM <ansurmehdi@gmail.com>
2020-12-25 22:57:24 +0500

    sync week 2 code

---
+++ week_02/mini_max_sum.js
@@ -0,0 +1,7 @@
+// Complete the miniMaxSum function below.
+function miniMaxSum(arr) {
+
+    // create an array for holding sums of each index in the index which was skipped
+    // loop over each index of input array
+    // use reduce with index 
+}
